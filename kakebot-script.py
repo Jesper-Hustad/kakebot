967e1e38e4ab78d4fcac649efbd359cbb1244dfb
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-09-17 15:29:18 +0200

    forenklet

--- kakebot-script.py
+++ kakebot-script.py
@@ -2,118 +2,67 @@ import os
 import requests
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-import sys
-import threading
 from dotenv import load_dotenv
-from window import open_html_fullscreen
-# import vlc
-import os
 
+load_dotenv()
 
-load_dotenv()  # Load environment variables from .env file
-
-
-def play_audio():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_dir, "cookie.mp3")
-    os.system(f'(sleep 5 && cvlc --intf dummy --play-and-exit --gain 5 {file_path})&')
-    # player = vlc.MediaPlayer(file_path)
-    # player.play()
-        
 print("Starting Slack kake bot app...")
 print("Bot Token:", os.getenv("SLACK_BOT_TOKEN"))
 print("App Token:", os.getenv("SLACK_APP_TOKEN"))
 
 def generate_slackbot_html(text, file_name):
     text = text or "(Ingen tekst inkludert i slack-meldingen)"
-    file_name = file_name or ""
-    bilde_html = f"""<img class="image" src="{file_name}" alt="Custom Image">""" if file_name else "<h3>[Ingen bilde i slack-meldingen]</h3>"
+    bilde_html = f'<img class="image" src="{file_name}" alt="Custom Image">' if file_name else "<h3>[Ingen bilde i slack-meldingen]</h3>"
+    
+    with open("template.html", "r", encoding="utf-8") as f:
+        template = f.read()
 
-    html_content = f"""<html>
-        <head>
-            <style>
-                body {{
-                    margin: 0;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    height: 100vh;
-                    background-color: #f4f4f4;
-                    font-family: Arial, sans-serif;
-                }}
-                .text {{
-                    position: absolute;
-                    top: 10%;
-                    text-align: center;
-                    font-size: 3em;
-                    color: #333;
-                }}
-                .image {{
-                    position: absolute;
-                    top: 20%;
-                    width: auto;
-                    height: 80%;
-                }}
-            </style>
-        </head>
-        <body>
-            <div class="text">{text}</div>
-            {bilde_html}
-        </body>
-    </html>"""
-    return html_content
-
+    with open("index.html", "w", encoding="utf-8") as f:
+        f.write(template.replace("{ TEKST }", text).replace("{ BILDE }", bilde_html))
 
 def download_image(image_url, token):
     try:
-        file_type = image_url.split(".")[-1]
-        file_name = f"image.{file_type}"
+        file_name = f"image.{image_url.split('.')[-1]}"
         response = requests.get(image_url, headers={"Authorization": f"Bearer {token}"})
         if response.status_code == 200:
-            with open(file_name, "wb") as image_file:
-                image_file.write(response.content)
+            with open(file_name, "wb") as f:
+                f.write(response.content)
             return file_name
-        else:
-            return ""
-    except Exception as e:
-        return ""
+    except:
+        pass
+    return ""
 
 def activate_kakebot(text, file_name):
-    html_content = generate_slackbot_html(text, file_name)
-    # write to index.html locally
-    with open("index.html", "w", encoding="utf-8") as file:
-        file.write(html_content)
-    play_audio()
-    open_html_fullscreen("index.html")
-    
+    generate_slackbot_html(text, file_name)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.system(f'(sleep 2 && cvlc --intf dummy --play-and-exit --gain 5 {script_dir}/cookie.mp3)&')
+    os.system(f'firefox --new-tab "file://{script_dir}/index.html" && sleep 160 && wtype -m ctrl w -M ctrl')
+    print("Aktiverer kakebot...")
+
+def er_kake(text, image_url):
+    return (image_url or "kake" in text.lower()) and ("http" not in text.lower() and "allmøte" not in text.lower())
 
 slack_app = App(token=os.getenv("SLACK_BOT_TOKEN"))
 
 @slack_app.event("message")
 def handle_message_events(body, logger):
     print("Received message event:", body)
-
-    text = body.get("event", {}).get("text", "")
-    image_url = body.get("event", {}).get("files", [{}])[0].get("url_private", "")
-    file_name = ""
-    if image_url:
-        file_name = download_image(image_url, os.getenv("SLACK_BOT_TOKEN"))
-
-    if (image_url or "kake" in text.lower()) and ("http" not in text.lower() and "allmøte" not in text.lower()):
+    event = body.get("event", {})
+    text = event.get("text", "")
+    image_url = event.get("files", [{}])[0].get("url_private", "")
+    
+    if er_kake(text, image_url):
+        file_name = download_image(image_url, os.getenv("SLACK_BOT_TOKEN")) if image_url else ""
         activate_kakebot(text, file_name)
 
 def start_slack_app():
     try:
         SocketModeHandler(slack_app, os.environ["SLACK_APP_TOKEN"]).start()
-    except Exception as e:
+    except:
         pass
 
 def main():
-    try:
-        start_slack_app()
-    except Exception as e:
-        pass
+    start_slack_app()
 
 if __name__ == "__main__":
     main()
